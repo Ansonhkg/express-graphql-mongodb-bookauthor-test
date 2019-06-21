c69ce9e0ec6ce9c78a1ebc55a913a23fb0c18083
Anson Cheung <ansoncwork@gmail.com>
2019-06-21 14:46:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+const app = express();
+
+// Connect to mongodb
+// make sure to replace my db string & credential with your own
+mongoose.connect('mongodb://admin:admin@localhost:27017/graphql', {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB.")
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+const PORT = process.env.port || 4000
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
